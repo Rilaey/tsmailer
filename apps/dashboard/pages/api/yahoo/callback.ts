5d0831b13746a45624c120df70b44fbe0acff6ee
Riley <94302477+Rilaey@users.noreply.github.com>
2024-08-22 21:21:45 -0400

    Added logic to save yahoo email accounts

--- apps/dashboard/pages/api/yahoo/callback.ts
+++ apps/dashboard/pages/api/yahoo/callback.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
 
 const YAHOO_CLIENT_ID = process.env.YAHOO_CLIENT_ID;
 const YAHOO_CLIENT_SECRET = process.env.YAHOO_CLIENT_SECRET;
@@ -37,10 +38,36 @@ export default async function callback(
     }
 
     const data = await response.json();
-    const { access_token, refresh_token } = data;
-    console.log(data);
-    // Save the tokens to the database
-    // Example: await saveTokensToDB(userId, { access_token, refresh_token });
+
+    const decodedTokenResponse = await fetch(
+      `https://api.login.yahoo.com/openid/v1/userinfo`,
+      {
+        headers: {
+          Authorization: `Bearer ${data.access_token}`
+        }
+      }
+    );
+
+    const decodedTokenJson = await decodedTokenResponse.json();
+
+    const clientPromise = MongoClient.connect(
+      process.env.MONGODB_URI as string
+    );
+
+    const db = (await clientPromise).db();
+
+    const currentDate = new Date();
+
+    await db.collection("emailAccounts").insertOne({
+      //@ts-ignore
+      userId: session.id,
+      provider: "Yahoo",
+      email: decodedTokenJson.email,
+      accessToken: data.access_token,
+      refreshToken: data.refresh_token,
+      createdDate: currentDate.toISOString(),
+      lastModifiedDate: currentDate.toISOString()
+    });
 
     res.redirect("/"); // Redirect to the dashboard or any other page
   } catch (error) {
